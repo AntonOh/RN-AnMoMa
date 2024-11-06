f6c75c2059270849e0f68b3ebc2e5078dd3a022f
scriptus longus <scriptus-longus@protonmail.com>
2024-11-06 01:26:46 +0100

    ugly solution to http checking

--- 1praxis/webserver.c
+++ 1praxis/webserver.c
@@ -2,9 +2,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <sys/socket.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <string.h>
+
 
 #define MAX_MEM_SIZE 8192
 #define CON_LIMIT 20
@@ -14,15 +17,46 @@ void zombie_handler() {
   while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-void con_handler(int client_fd, void* accept_pack_mem, size_t mem_size) {
-  for(;;) {
-    //size_t n = recv(client_fd, accept_pack_mem, 8191, 0);  
+size_t get_line_idx(char mem[], size_t mem_size) {
+  for (size_t i = 0; i < mem_size-1; i++) {
+    if (mem[i] == '\r' && mem[i+1] == '\n') {
+      return i;
+    }
+  }
+}
 
+void con_handler(int client_fd, void* accept_pack_mem, size_t mem_size) {
+  // to make sure we lose no date when payload is large
+  bool got_method = false;
+  bool got_header = false;
+  for(;;) {
+    memset(accept_pack_mem, 0x0, mem_size);
     if (recv(client_fd, accept_pack_mem, mem_size, 0) == -1 ) {
       printf("Error recv");
       exit(0);
     }
-    send(client_fd, "Reply", 5, 0);   
+    //send(client_fd, "Reply", 9, 0);
+
+    size_t n = 0;
+    size_t idx = 0;
+    while (idx < mem_size) {
+      n = get_line_idx(&(((char *)accept_pack_mem)[idx]), mem_size);
+      idx = idx + n + 2;
+
+      if (got_header == true ) {
+        got_method = false;
+        got_header = false;
+        send(client_fd, "Reply\r\n\r\n", 9, 0);
+      }
+
+      if (got_method == false ) {
+        got_method = true;
+      } else {
+        if (n == 0 && got_header == false) {
+          got_header = true;
+        } 
+      }
+    }
   }   
 }
 
