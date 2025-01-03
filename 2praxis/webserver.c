52b5862a3585bc388b6a4b207c95e537bee98170
Anton_Ohler <anton.ohler@gmail.com>
2025-01-03 16:31:22 +0100

    calculate uri hash

--- 2praxis/webserver.c
+++ 2praxis/webserver.c
@@ -70,6 +70,9 @@ void send_reply(int conn, struct request *request) {
     fprintf(stderr, "Handling %s request for %s (%lu byte payload)\n",
             request->method, request->uri, request->payload_length);
 
+    //calculate hash of resource path
+    uint16_t uri_hash = pseudo_hash(request->uri, strlen(request->uri));
+
     if (strcmp(request->method, "GET") == 0) {
         // Find the resource with the given URI in the 'resources' array.
         size_t resource_length;
