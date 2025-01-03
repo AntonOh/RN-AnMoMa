da3d73837589f2bf299c5722a62ed634d8ad8a64
Anton_Ohler <anton.ohler@gmail.com>
2025-01-03 10:54:08 +0100

    reset

--- 2praxis/webserver.c
+++ 2praxis/webserver.c
@@ -96,19 +96,14 @@ void send_reply(int conn, struct request *request) {
  * @return Returns the number of bytes processed from the packet.
  *         If the packet is successfully processed and a reply is sent, the
  * return value indicates the number of bytes processed. If the packet is
- * malformed or an error occurs during processing, the return value is -1.  
+ * malformed or an error occurs during processing, the return value is -1.
  *
  */
 size_t process_packet(int conn, char *buffer, size_t n) {
     struct request request = {
         .method = NULL, .uri = NULL, .payload = NULL, .payload_length = -1};
     ssize_t bytes_processed = parse_request(buffer, n, &request);
-    uint16_t hash = pseudo_hash(request.uri, strlen(request.uri));
-    if (hash>own_id && hash>=successor_id){ //<-should be true if current node is not responsible for that uri
-        //skip everything and send give the successor the task via udp package
-        send(to_successor, take care of this shit)
-        
-    }
+
     if (bytes_processed > 0) {
         send_reply(conn, &request);
 
@@ -229,7 +224,7 @@ static struct sockaddr_in derive_sockaddr(const char *host, const char *port) {
     // Resolve the host (IP address or hostname) into a list of possible
     // addresses.
     int returncode = getaddrinfo(host, port, &hints, &result_info);
-    if (returncode) { //remove this later for one line less yippieee
+    if (returncode) {
         fprintf(stderr, "Error parsing host/port");
         exit(EXIT_FAILURE);
     }
@@ -250,26 +245,24 @@ static struct sockaddr_in derive_sockaddr(const char *host, const char *port) {
  *
  * @return The file descriptor of the created TCP server socket.
  */
-static int setup_server_socket(struct sockaddr_in addr, int __type) {
+static int setup_server_socket(struct sockaddr_in addr) {
     const int enable = 1;
     const int backlog = 1;
 
     // Create a socket
-    int sock = socket(AF_INET, __type, 0);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         perror("socket");
         exit(EXIT_FAILURE);
     }
 
-    if (__type == SOCK_STREAM) {
-        // Avoid dead lock on connections that are dropped after poll returns but
-        // before accept is called
-        if (fcntl(sock, F_SETFL, O_NONBLOCK) == -1) {
-            perror("fcntl");
-            exit(EXIT_FAILURE);
-        }
+    // Avoid dead lock on connections that are dropped after poll returns but
+    // before accept is called
+    if (fcntl(sock, F_SETFL, O_NONBLOCK) == -1) {
+        perror("fcntl");
+        exit(EXIT_FAILURE);
     }
-    
+
     // Set the SO_REUSEADDR socket option to allow reuse of local addresses
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(enable)) ==
         -1) {
@@ -284,13 +277,11 @@ static int setup_server_socket(struct sockaddr_in addr, int __type) {
         exit(EXIT_FAILURE);
     }
 
-    if (__type == SOCK_STREAM) {
-        // Start listening on the socket with maximum backlog of 1 pending
-        // connection
-        if (listen(sock, backlog)) {
-            perror("listen");
-            exit(EXIT_FAILURE);
-        }
+    // Start listening on the socket with maximum backlog of 1 pending
+    // connection
+    if (listen(sock, backlog)) {
+        perror("listen");
+        exit(EXIT_FAILURE);
     }
 
     return sock;
@@ -299,10 +290,9 @@ static int setup_server_socket(struct sockaddr_in addr, int __type) {
 /**
  *  The program expects 3; otherwise, it returns EXIT_FAILURE.
  *
- *  Call as (with exemplary values):
+ *  Call as:
  *
- *  PRED_ID=49152 PRED_IP=127.0.0.1 PRED_PORT=2002 SUCC_ID=49152 SUCC_IP=127.0.0.1
-    SUCC_PORT=2002 ./build/webserver 127.0.0.1 2001 16384
+ *  ./build/webserver self.ip self.port
  */
 int main(int argc, char **argv) {
     if (argc != 3) {
@@ -311,13 +301,12 @@ int main(int argc, char **argv) {
 
     struct sockaddr_in addr = derive_sockaddr(argv[1], argv[2]);
 
-    // Set up a UDP and TCP server socket.
-    int udp_server_socket = setup_server_socket(addr, SOCK_DGRAM);
-    int tcp_server_socket = setup_server_socket(addr, SOCK_STREAM);
+    // Set up a server socket.
+    int server_socket = setup_server_socket(addr);
 
     // Create an array of pollfd structures to monitor sockets.
     struct pollfd sockets[2] = {
-        {.fd = tcp_server_socket, .events = POLLIN},
+        {.fd = server_socket, .events = POLLIN},
     };
 
     struct connection_state state = {0};
@@ -339,14 +328,14 @@ int main(int argc, char **argv) {
             }
             int s = sockets[i].fd;
 
-            if (s == tcp_server_socket) {
+            if (s == server_socket) {
 
-                // If the event is on the tcp_server_socket, accept a new connection
+                // If the event is on the server_socket, accept a new connection
                 // from a client.
-                int connection = accept(tcp_server_socket, NULL, NULL);
+                int connection = accept(server_socket, NULL, NULL);
                 if (connection == -1 && errno != EAGAIN &&
                     errno != EWOULDBLOCK) {
-                    close(tcp_server_socket);
+                    close(server_socket);
                     perror("accept");
                     exit(EXIT_FAILURE);
                 } else {
